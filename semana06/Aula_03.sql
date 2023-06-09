b26948a91c3d23dd86a0a7c0807341bf766fcea3
Rawan.H <rawanhawangledt@gmail.com>
2023-06-09 07:11:52 -0300

    Aula 03
    Exercício Resolvido em Sala com Draw.io e Script SQL

---
+++ semana06/Aula_03.sql
@@ -0,0 +1,42 @@
+create type categoriaEnum as ENUM('Conferência', 
+								  'Seminário',
+								  'Workshop');
+
+create table local (
+	id SERIAL primary key,
+	nome varchar(100),
+	endereco varchar(100),
+	capacidade integer,
+	check (capacidade >= 1)
+);
+
+create table Categoria (
+	id SERIAL primary key,
+	nome categoriaEnum,
+	descricao varchar(100)	
+);
+
+create table participante (
+	id SERIAL primary key,
+	nome varchar(100),
+	email varchar(100)
+);
+
+create table evento (
+	id SERIAL primary key,
+	nome varchar(100),
+	data Date,
+	local_id Integer,
+	categoria_id Integer,
+	ativo Boolean default true,
+	FOREIGN KEY (local_id) REFERENCES Local(ID),
+	FOREIGN KEY (categoria_id) REFERENCES Categoria(ID),
+	check (data > current_date)
+);
+
+create table ParticipantesEventos (
+	participante_id Integer,
+	evento_id Integer,
+	FOREIGN key (participante_id) references Participante(Id),
+	FOREIGN key (evento_id) references Evento(Id)
+);
