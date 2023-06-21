9c691be436b2fe2a3c8f2b1943b5cac3c5e797b3
pedrohbsilva <phbs1235@gmail.com>
2023-06-20 21:30:59 -0300

    feat: semana 08 aula 02

---
+++ semana08/backend/src/config/database.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  dialect: 'postgres', //Qual banco de dados está utilizando;
+  host: 'localhost', //Qual servidor está utilizando;
+  username: 'postgres', //Qual o nome do seu usuário no postgres;
+  password: 'postgres', //Qual a senha do seu usuário no postgres;
+  database: 'postgres', //Qual o nome do seu database no postgres;
+  port: 5432 //Qual porta do seu postgres (Normalmente é a 5432);
+};
